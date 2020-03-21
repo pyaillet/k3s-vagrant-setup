8f93e58aae3efa27d5b91c409f7fa345e10f95b4
Pierre-Yves Aillet <pyaillet@gmail.com>
2020-03-21 21:10:12 +0100

    :bug: fix k3s download url

--- up.sh
+++ up.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -LO https://github.com/rancher/k3s/releases/download/latest/k3s
+curl -LO https://github.com/rancher/k3s/releases/latest/download/k3s
 chmod +x k3s
 
 ssh-keygen -t rsa -f ./id_rsa -q -N ""
